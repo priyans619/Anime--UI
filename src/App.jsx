abd53c350729b5b5a021fc62be157d81daf3ab3d
priyans619 <priyansh.2b@gmail.com>
2024-04-30 23:59:56 +0530

    Add title

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import TopAnime from './components/TopAnime';
 const App = () => {
   return (
     <div className=" bg-orange-50 mx-auto px-4 py-8">
+      <h1 className="text-3xl text-center font-bold mb-4">Top 20 Anime</h1>
       <TopAnime />
     </div>
   );
