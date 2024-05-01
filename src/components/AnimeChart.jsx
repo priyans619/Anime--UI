4f842922e23402b8f9d8f988cf97044446bb2fe3
priyans619 <priyansh.2b@gmail.com>
2024-05-01 19:06:44 +0530

    render AreaChart in X,Y-axis

--- src/components/AnimeChart.jsx
+++ src/components/AnimeChart.jsx
@@ -52,7 +52,19 @@ const ChartComponent = () => {
   return (
     <div style={{ width: '100%', height: 400 }}>
       <ResponsiveContainer>
-        
+      <AreaChart data={animeData}>
+          <defs>
+            <linearGradient id="colorGradient" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+            </linearGradient>
+          </defs>
+          <XAxis dataKey="year" />
+          <YAxis type="number" />
+          <CartesianGrid strokeDasharray="3 3" />
+          <Tooltip content={<CustomTooltip />} />
+          <Area type="monotone" dataKey="anime.length" stroke="#8884d8" fillOpacity={1} fill="url(#colorGradient)" />
+        </AreaChart>
       </ResponsiveContainer>
     </div>
   );
