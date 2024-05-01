25c1c502090345d79f65002b21beb14b6e2e26ef
priyans619 <priyansh.2b@gmail.com>
2024-05-01 15:19:54 +0530

    Anime card size

--- src/components/TopAnime.jsx
+++ src/components/TopAnime.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 
 const TopAnime20 = () => {
   const [topAnime, setTopAnime] = useState([]);
+  const [expandedCard, setExpandedCard] = useState(null);
 
   useEffect(() => {
     const fetchTopAnime = async () => {
@@ -23,18 +24,25 @@ const TopAnime20 = () => {
     fetchTopAnime();
   }, []);
 
+
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 p-12 relative">
       {topAnime.map((anime) => (
-        <div key={anime.mal_id} className="relative bg-white rounded-lg shadow-md">
+        <div
+          key={anime.mal_id}
+          className={`relative p-1 transition-transform duration-300 ease-out overflow-hidden ${expandedCard === anime.mal_id ? 'z-30 scale-125' : 'z-0 transform scale-100'
+            }`}
+
+        >
           <span className="absolute bg-pink-500 rounded-tr-lg px-2 py-1 top-0 right-0 text-sm font-bold text-gray-600">{`${anime.rank}`}</span>
           <img
             src={anime.images.jpg.image_url}
             alt={anime.title}
             className="rounded-t-lg h-48 w-full object-cover"
-            style={{ height: '250px' }}
+            style={{ height: '300px' }}
           />
-          <h2 className="text-lg text-center font-bold mt-2 overflow-hidden line-clamp-2">{anime.title}</h2>
+          <h2 className="text-lg text-center font-bold overflow-hidden line-clamp-2">{anime.title}</h2>
+          
         </div>
       ))}
     </div>
