854fe3bfe51b60bce2c5a783d6411905fc0d93a7
priyans619 <priyansh.2b@gmail.com>
2024-05-01 15:24:18 +0530

    ExpandedCard details

--- src/components/TopAnime.jsx
+++ src/components/TopAnime.jsx
@@ -52,7 +52,13 @@ const TopAnime20 = () => {
             style={{ height: '300px' }}
           />
           <h2 className="text-lg text-center font-bold overflow-hidden line-clamp-2">{anime.title}</h2>
-          
+          {expandedCard === anime.mal_id && (
+            <div className="text-sm p-2 absolute bottom-0 left-0 right-0 bg-white rounded-b-lg">
+              <p><span className="">Release Date:</span> {anime.aired.from}</p>
+              <p><span className="">Latest Date:</span> {anime.aired.to || 'Now'}</p>
+              <p><span className="">Rated:</span> {anime.rating}</p>
+            </div>
+          )}
         </div>
       ))}
     </div>
