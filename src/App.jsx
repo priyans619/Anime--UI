36cd77de6b49ef8fa6c68a83d744d88622005b87
priyans619 <priyansh.2b@gmail.com>
2024-05-01 19:12:14 +0530

    render AnimeChart component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import TopAnime from './components/TopAnime';
+import AnimeChart from './components/AnimeChart';
 
 const App = () => {
   return (
     <div className=" bg-orange-50 mx-auto px-4 py-8">
-      <h1 className="text-3xl text-center font-bold mb-4">Top 20 Anime</h1>
+      <h1 className="text-3xl text-center font-bold">Top 20 Anime</h1>
       <TopAnime />
+      <AnimeChart />
     </div>
   );
 };
